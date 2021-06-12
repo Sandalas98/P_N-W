47638edec2383a1ad35467739848f2a2b5131bd3
lsiemanski <l.siemanski@gmail.com>
2021-06-12 17:45:14 +0200

    Analiza

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # disable tensorflow warnings
 
-from statistics import perform_ttest
+from statistics import perform_wilcoxon_test
 
 import numpy as np
 from keras.wrappers.scikit_learn import KerasRegressor
@@ -11,7 +12,9 @@ from sklearn.svm import SVR
 from nn import create_model
 from util import load_data
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # disable tensorflow warnings
+import eli5
+from eli5.sklearn import PermutationImportance
+
 
 data = load_data()
 
@@ -29,7 +32,17 @@ kNN_metric = ['euclidean', 'manhattan']
 svr_param_C = [0.25, 0.5, 1, 2, 3, 4, 5]
 svr_degree = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-cv = RepeatedKFold(n_splits=5, n_repeats=2, random_state=71)
+# svr_param_C = [0.25]
+# svr_degree = [3]
+#
+# kNN_weights = ['uniform']
+# kNN_neighbors = [5]
+# kNN_metric = ['euclidean']
+
+n_splits = 5
+n_repeats = 2
+
+cv = RepeatedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=71)
 
 kNN_scores = {}
 svm_scores = {}
@@ -55,8 +68,8 @@ for weight in kNN_weights:
     for neighbor in kNN_neighbors:
         for metric in kNN_metric:
             clf = KNeighborsRegressor(n_neighbors=neighbor, weights=weight, metric=metric)
-            score = cross_val_score(clf, X, Y, scoring='neg_mean_absolute_error', cv=cv)
-            kNN_scores["w={0},k={1},m={2}".format(weight[0], neighbor, metric[0])] = list(score)
+            score = cross_val_score(clf, X, Y, scoring='neg_mean_squared_error', cv=cv)
+            kNN_scores["w={0},k={1},m={2}".format(weight[0], neighbor, metric[0])] = score
             mean_of_scores = np.mean(score)
             if mean_of_scores > best_kNN["best_value"]:
                 best_kNN["best_value"] = mean_of_scores
@@ -72,8 +85,8 @@ for weight in kNN_weights:
 for c in svr_param_C:
     for degree in svr_degree:
         clf = SVR(C=c, degree=degree)
-        score = cross_val_score(clf, X, Y, scoring='neg_mean_absolute_error', cv=cv)
-        svm_scores["SVM, c={0}, degree={1}".format(c, degree)] = list(score)
+        score = cross_val_score(clf, X, Y, scoring='neg_mean_squared_error', cv=cv)
+        svm_scores["SVM, c={0}, degree={1}".format(c, degree)] = score
         mean_of_scores = np.mean(score)
         if mean_of_scores > best_SVM["best_value"]:
             best_SVM["best_value"] = mean_of_scores
@@ -82,26 +95,33 @@ for c in svr_param_C:
             best_SVM["degree"] = degree
 
 hidden_activations = ['sigmoid', 'relu', 'tanh']
-# hidden_layer_neurons = [1, 2, 5, 10, 15, 20, 25, 50, 100, 200, 500, 1000, 2500, 5000, 10000]
-hidden_layer_neurons = [10]
+hidden_layer_neurons = [1, 2, 5, 10, 15, 20, 25, 50, 100, 200, 500, 1000, 2500, 5000, 10000]
 loss_functions = ['mean_squared_error', 'mean_absolute_error']
+
+# hidden_activations = ['relu']
+# hidden_layer_neurons = [200]
+# loss_functions = ['mean_squared_error']
 
 best_NN = {
     "best_value": -1,
     "samples": [],
     "hidden_activation": -1,
     "hidden_layer_neurons": -1,
-    "loss_function": -1
+    "loss_function": -1,
+    "model" : None
 }
 
-for hidden_activation in hidden_activations:
-    for hid_layer_neurons in hidden_layer_neurons:
-        for loss in loss_functions:
+nn_scores_arr = np.zeros((len(hidden_activations), len(hidden_layer_neurons), len(loss_functions), n_splits*n_repeats))
+
+for i, hidden_activation in enumerate(hidden_activations):
+    for j, hid_layer_neurons in enumerate(hidden_layer_neurons):
+        for k, loss in enumerate(loss_functions):
             nn = create_model(X.shape[1], hidden_layer_neurons=hid_layer_neurons,
                               hidden_activation=hidden_activation, loss=loss)
             clf = KerasRegressor(build_fn=lambda: nn, epochs=25, verbose=0)
-            score = cross_val_score(clf, X, Y, scoring='neg_mean_absolute_error', cv=cv)
-            nn_scores["NN, activation={0}, neurons={1}, loss={2}".format(hidden_activation, hid_layer_neurons, loss)] = list(score)
+            score = cross_val_score(clf, X, Y, scoring='neg_mean_squared_error', cv=cv)
+            nn_scores["NN, activation={0}, neurons={1}, loss={2}".format(hidden_activation, hid_layer_neurons, loss)] = score
+            nn_scores_arr[i][j][k][:] = score
             mean_of_scores = np.mean(score)
             if mean_of_scores > best_NN["best_value"]:
                 best_NN["best_value"] = mean_of_scores
@@ -109,11 +129,25 @@ for hidden_activation in hidden_activations:
                 best_NN["hidden_activation"] = hidden_activation
                 best_NN["hidden_layer_neurons"] = hid_layer_neurons
                 best_NN["loss_function"] = loss
+                best_NN["model"] = clf
 
 # Wypisanie najlepszych:
 print(best_kNN)
 print(best_SVM)
 print(best_NN)
 
-# Test t-Studenta:
-perform_ttest([list(best_kNN["samples"]), list(best_SVM["samples"]), list(best_NN["samples"])], ["kNN", "SVM", "NN"])
+# Test Wilcoxona:
+perform_wilcoxon_test(np.array([best_kNN["samples"], best_SVM["samples"], best_NN["samples"]]), ["kNN", "SVM", "NN"])
+
+np.save('nn_scores', nn_scores_arr)
+
+clf = best_NN["model"]
+clf.fit(X, Y)
+perm = PermutationImportance(clf, random_state=1).fit(X, Y) # https://eli5.readthedocs.io/en/latest/blackbox/permutation_importance.html - opis jak to działa
+
+with open('results.htm', 'wb') as f:
+    f.write(eli5.show_weights(perm, feature_names=X.columns.tolist()).data.encode("UTF-8"))
+
+print(kNN_scores)
+print(svm_scores)
+print(nn_scores)
--- statistics.py
+++ statistics.py
@@ -1,28 +1,42 @@
 import numpy as np
-from scipy.stats import wilcoxon
+from scipy.stats import wilcoxon, ranksums
 from tabulate import tabulate
 
-def perform_ttest(scores, headers, alfa=.05):
-    w_statistic = np.zeros((len(scores), len(scores)))
-    p_value = np.zeros((len(scores), len(scores)))
+from scipy.stats import rankdata
 
-    for i in range(len(scores)):
-        for j in range(len(scores)):
-            w_statistic[i, j], p_value[i, j] = wilcoxon(scores[i], scores[j], zero_method='zsplit')
+def perform_wilcoxon_test(scores, headers, alfa=.05):
+    print(scores)
+
+    scores = scores.T
+
+    ranks = []
+    for ms in scores:
+        ranks.append(rankdata(ms).tolist())
+    ranks = np.array(ranks)
+    print("\nRanks:\n", ranks)
+
+    clfs_number = len(scores[0])
+    w_statistic = np.zeros((clfs_number, clfs_number))
+    p_value = np.zeros((clfs_number, clfs_number))
+
+    for i in range(clfs_number):
+        for j in range(clfs_number):
+            w_statistic[i, j], p_value[i, j] = ranksums(ranks.T[i], ranks.T[j])
+
     names_column = np.array([[header] for header in headers])
     w_statistic_table = np.concatenate((names_column, w_statistic), axis=1)
     w_statistic_table = tabulate(w_statistic_table, headers, floatfmt=".2f")
     p_value_table = np.concatenate((names_column, p_value), axis=1)
     p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
-    print("t-statistic:\n", w_statistic_table, "\n\np-value:\n", p_value_table)
+    print("w-statistic:\n", w_statistic_table, "\n\np-value:\n", p_value_table)
 
-    advantage = np.zeros((len(scores), len(scores)))
+    advantage = np.zeros((clfs_number, clfs_number))
     advantage[w_statistic > 0] = 1
     advantage_table = tabulate(np.concatenate(
         (names_column, advantage), axis=1), headers)
     print("Advantage:\n", advantage_table)
 
-    significance = np.zeros((len(scores), len(scores)))
+    significance = np.zeros((clfs_number, clfs_number))
     significance[p_value <= alfa] = 1
     significance_table = tabulate(np.concatenate(
         (names_column, significance), axis=1), headers)
